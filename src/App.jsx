e3f35bddf484aff51b51d60bc6244d6436cff71f
gammaseeker <joeyjiem@gmail.com>
2021-01-26 15:29:58 -0500

    added issue table

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,62 @@
-class IssueFilter extends React.Component{
-    render(){
-        return(
-            <div>placeholder for issue filter</div>
-        );
-    }
-}
-
-class IssueTable extends React.Component{
-    render(){
-        return(
-            <div>placeholder for issue table</div>
-        );
-    }
-}
-
-class IssueAdd extends React.Component{
-    render(){
-        return(
-            <div>placeholder for issue add</div>
-        );
-    }
-}
-
-class IssueList extends React.Component {
+class IssueFilter extends React.Component {
     render() {
       return (
-        <div>
+        <div>This is a placeholder for the issue filter.</div>
+      );
+    }
+  }
+
+  class IssueRow extends React.Component {
+      render() {
+        return (
+            <td>{this.props.issue_title}</td>
+        )
+      }
+
+  }
+  
+  class IssueTable extends React.Component {
+    render() {
+      return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Title</th>
+                </tr>
+            </thead>
+            <tbody>
+                <IssueRow issue_title="Title"/> 
+                <IssueRow issue_title="Title"/>
+            </tbody>
+        </table>
+      );
+    }
+  }
+  
+  class IssueAdd extends React.Component {
+    render() {
+      return (
+        <div>This is a placeholder for a form to add an issue.</div>
+      );
+    }
+  }
+  
+  class IssueList extends React.Component {
+    render() {
+      return (
+        <React.Fragment>
           <h1>Issue Tracker</h1>
           <IssueFilter />
           <hr />
           <IssueTable />
           <hr />
           <IssueAdd />
-        </div>
+        </React.Fragment>
       );
     }
-}
-
-class HelloWorld extends React.Component{
-    render(){
-        const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-        const helloContinents = Array.from(continents, c=>`Hello ${c}!`);
-        const message = helloContinents.join(' ');
-
-        return(
-            <div title="Outer div">
-                <h1>{message}</h1>
-            </div>
-        );
-    }
-}
-//const element = <HelloWorld />;
-const contentNode = document.getElementById('contents');
-//ReactDOM.render(element, document.getElementById('contents'));
-ReactDOM.render(<IssueList />, contentNode);    // Render the component inside the content Node
+  }
+  
+  const element = <IssueList />;
+  
+  ReactDOM.render(element, document.getElementById('contents'));
